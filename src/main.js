6d996e47275f415bb4619f434601c94ad0a581cb
Haymanot <235764709+haymanotkefyalew@users.noreply.github.com>
2026-03-27 22:08:45 +0300

    Feat:add a code which  create project and append it

--- src/main.js
+++ src/main.js
@@ -1,6 +1,7 @@
 const addNewCategory=document.querySelector(".addNewCategory")
 const saveProjects=document.querySelector(".save-project")
 const projectName=document.querySelector("#project-name")
+const projectDescription=document.querySelector("#project-description")
 const categories=document.querySelector(".categories")
 const newProjectM=document.querySelector(".projects-modal-none")
 
@@ -9,8 +10,17 @@ addNewCategory.addEventListener("click",()=>{
 })
 
 saveProjects.addEventListener("click",()=>{
-    const newCategory=document.createElement("h4")
-    newCategory.textContent=projectName.value
+    const newCategoryTitle=document.createElement("summary")
+    newCategoryTitle.classList.add("project-title")
+    newCategoryTitle.textContent=projectName.value
+    const newCategoryDesc=document.createElement("p")
+    newCategoryDesc.classList.add("project-desc")
+    newCategoryDesc.textContent=projectDescription.value
+
+    const newCategory=document.createElement("details")
+    newCategory.classList.add("project")
+    newCategory.append(newCategoryTitle,newCategoryDesc)
+
     categories.insertBefore(newCategory,addNewCategory)
     newProjectM.classList.remove("flex")
 })

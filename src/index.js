5c919152184d511fa5251e57b99a041b2f336892
Haymanot <235764709+haymanotkefyalew@users.noreply.github.com>
2026-03-27 10:49:07 +0300

    Feat: add an event listner to the components

--- src/index.js
+++ src/index.js
@@ -3,15 +3,20 @@ const logInBtnH=document.querySelectorAll(".hbtn1");
 const logInModal=document.querySelector(".login-modal-none")
 const signUpBtnN=document.querySelectorAll(".nbtn2");
 const signUpBtnH=document.querySelectorAll(".hbtn2");
+const modalSubmit=document.querySelector(".modal-submit");
+const lModalSubmit=document.querySelector(".lmodal-submit");
+const signUpBtnC=document.querySelectorAll(".cta-component");
 const signUpModal=document.querySelector(".signup-modal-none")
 
 let logInButtons=[...logInBtnN,...logInBtnH]
-let signUpButtons=[...signUpBtnN,...signUpBtnH]
+let signUpButtons=[...signUpBtnN,...signUpBtnH,...signUpBtnC]
 
 logInButtons.forEach((each)=>{
     each.addEventListener("click",(e)=>{
+        document.body.classList.add("no-scroll")
     signUpModal.classList.remove("flex")
     logInModal.classList.add("flex")
+    
     e.preventDefault();
 })})
 
@@ -19,6 +24,21 @@ signUpButtons.forEach((each)=>{
     each.addEventListener("click",(e)=>{
     logInModal.classList.remove("flex")
     signUpModal.classList.add("flex")
+    document.body.classList.add("no-scroll")
     e.preventDefault();
 })}
 )
+
+modalSubmit.addEventListener("click",(e)=>{
+    document.body.classList.remove("no-scroll")
+    window.location.href="main.html"
+    signUpModal.classList.remove("flex")
+     e.preventDefault();
+})
+lModalSubmit.addEventListener("click",(e)=>{
+    document.body.classList.remove("no-scroll")
+    logInModal.classList.remove("flex")
+    window.location.href="main.html"
+     e.preventDefault();
+})
+

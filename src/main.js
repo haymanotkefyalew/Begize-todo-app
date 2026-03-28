9a91567d06c418326c16f224eb99cd539b2edebf
Haymanot <235764709+haymanotkefyalew@users.noreply.github.com>
2026-03-28 16:33:47 +0300

    Feat: add event listner to the activities inside setupEventListeners function

--- src/main.js
+++ src/main.js
@@ -9,4 +9,9 @@ function setupEventListeners(){
 
     document.querySelector('.cancel-tmodal').addEventListener('click', closeModals);
     document.querySelector('.save-task').addEventListener('click', addTask);
+
+    const activityBtns = document.querySelectorAll('.activities button');
+    activityBtns[0].addEventListener('click', () => setView('all'));
+    activityBtns[1].addEventListener('click', () => setView('pending'));
+    activityBtns[2].addEventListener('click', () => setView('completed'));
 }

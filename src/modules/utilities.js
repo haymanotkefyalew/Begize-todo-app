9e2f6bf9b5624b508dfe6e192231ae34c89e0a96
Haymanot <235764709+haymanotkefyalew@users.noreply.github.com>
2026-03-29 00:22:41 +0300

    Feat: add utilities module which give utility functions

---
+++ src/modules/utilities.js
@@ -0,0 +1,10 @@
+export function clearWindowS(){
+    sFullName.value="";
+    sPass.value=""
+    sPassC.value=""
+    sUsername.value=""
+}
+export function clearWindowL(){
+    lPass.value=""
+    lUsername.value=""
+}

4a950d6c674d370e8170806d3db0465d6e65ead6
Haymanot <235764709+haymanotkefyalew@users.noreply.github.com>
2026-03-28 16:33:02 +0300

    Feat: add event listner to the tasks modal inside setupEventListeners function

--- src/main.js
+++ src/main.js
@@ -6,4 +6,7 @@ function setupEventListeners(){
     document.querySelector('.addNewCategory').addEventListener('click', () => openModal('projects'));
     document.querySelector('.cancel-pmodal').addEventListener('click', closeModals);
     document.querySelector('.save-project').addEventListener('click', addProject); 
+
+    document.querySelector('.cancel-tmodal').addEventListener('click', closeModals);
+    document.querySelector('.save-task').addEventListener('click', addTask);
 }

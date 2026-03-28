2cc3648d761ab73c866b43b159de4a45d3f8d7e3
Haymanot <235764709+haymanotkefyalew@users.noreply.github.com>
2026-03-28 16:31:42 +0300

    Feat: add event listner to the projects modal inside setupEventListeners function

--- src/main.js
+++ src/main.js
@@ -1,3 +1,9 @@
 let projects = [];
 let currentView = 'all'
 let selectedTaskId = null;
+
+function setupEventListeners(){
+    document.querySelector('.addNewCategory').addEventListener('click', () => openModal('projects'));
+    document.querySelector('.cancel-pmodal').addEventListener('click', closeModals);
+    document.querySelector('.save-project').addEventListener('click', addProject); 
+}
